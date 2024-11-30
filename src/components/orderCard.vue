b858ceb89c111f0e5d0169ee44b278f5a6ac11f2
SunburstMeoga <1025113986@qq.com>
2024-12-01 05:04:57 +0800

    订单

--- src/components/orderCard.vue
+++ src/components/orderCard.vue
@@ -4,99 +4,114 @@
 			<view class="info-title flex justify-between items-center">
 				<view class="title-left flex justify-start items-center">
 					<view class="order-word">订单编号</view>
-					<view class="order-content">{{orderNumber}}</view>
+					<view class="order-content">{{ orderNumber }}</view>
 				</view>
-				<view class="title-right">{{status === 3 ? '交易成功' : '交易失败'}}</view>
+				<view class="title-right">{{ status }}</view>
 			</view>
 			<view class="info-details flex justify-between items-center">
 				<view class="details-left">
 					<image :src="product_picture" mode="aspectFit"></image>
 				</view>
 				<view class="details-right flex justify-between items-end">
-					<view class="right-price">￥{{price.toFixed(2)}}</view>
-					<view class="right-count">共 {{count}} 件</view>
+					<view class="right-price">￥{{ price.toFixed(2) }}</view>
+					<view class="right-count">共 {{ count }} 件</view>
 				</view>
 			</view>
-			<view class="info-type">自提</view>
+			<view class="info-type">{{ pickUpMethod }}</view>
 		</view>
 	</view>
 </template>
 
 <script setup>
-	import {ref,defineProps} from 'vue'
-	const props = defineProps({
-		orderNumber: {
-			type: String,
-			default: ''
-		},
-		price: {
-			type: Number,
-			default: 0
-		},
-		count: {
-			type: Number,
-			default: 0
-		},
-		status: {
-			type: Number,
-			default: 0
-		},
-		product_picture: {
-			type: String,
-			default: ''
-		}
-	})
+import { ref, defineProps } from 'vue'
+const props = defineProps({
+	orderNumber: {
+		type: String,
+		default: ''
+	},
+	price: {
+		type: Number,
+		default: 0
+	},
+	count: {
+		type: Number,
+		default: 0
+	},
+	status: {
+		type: String,
+		default: ''
+	},
+	product_picture: {
+		type: String,
+		default: ''
+	},
+	pickUpMethod: {
+		type: String,
+		default: ''
+	}
+})
 </script>
 
 <style lang="scss" scoped>
-	.content {
-		width: 100%;
-		background-color: #fff;
-		padding: 28rpx 0;
-		.content-info {
-			width: 630rpx;
-			.info-title {
-				width: 100%;
-				font-size: 28rpx;
-				.title-left {
-					.order-word {
-						color: #595959;
-					}
-					.order-content {
-						color: #8c8c8c;
-						margin-left: 10rpx;
-					}
-				}
-				.title-right {
+.content {
+	width: 100%;
+	background-color: #fff;
+	padding: 28rpx 0;
+
+	.content-info {
+		width: 630rpx;
+
+		.info-title {
+			width: 100%;
+			font-size: 28rpx;
+
+			.title-left {
+				.order-word {
 					color: #595959;
 				}
-			}
-			.info-details {
-				width: 100%;
-				margin: 24rpx 0;
-				.details-left {
-					width: 100rpx;
-					height: 100rpx;
-					// border: 1px solid red;
-					border-radius: 10rpx;
-					overflow: hidden;
-				}
-				.details-right {
-					height: 100rpx;
-					flex-direction: column;
-					.right-price {
-						color: #222;
-						font-size: 32rpx;
-					}
-					.right-count {
-						color: #595959;
-						font-size: 24rpx;
-					}
+
+				.order-content {
+					color: #8c8c8c;
+					margin-left: 10rpx;
 				}
 			}
-			.info-type{
+
+			.title-right {
 				color: #595959;
 			}
 		}
+
+		.info-details {
+			width: 100%;
+			margin: 24rpx 0;
+
+			.details-left {
+				width: 100rpx;
+				height: 100rpx;
+				// border: 1px solid red;
+				border-radius: 10rpx;
+				overflow: hidden;
+			}
+
+			.details-right {
+				height: 100rpx;
+				flex-direction: column;
+
+				.right-price {
+					color: #222;
+					font-size: 32rpx;
+				}
+
+				.right-count {
+					color: #595959;
+					font-size: 24rpx;
+				}
+			}
+		}
+
+		.info-type {
+			color: #595959;
+		}
 	}
+}
 </style>
--- src/pages/personal/orders.vue
+++ src/pages/personal/orders.vue
@@ -7,12 +7,18 @@
     </route>
 <template>
 	<view class="container">
-		<div class=""></div>
+		<div class="w-686rpx mx-auto flex justify-between items-center h-116rpx">
+			<div class="rounded-8rpx px-16rpx py-8rpx text-28rpx" @click="handleType(item)"
+				:class="selectType === item.status ? 'bg-slate9 text-amber font-medium' : 'bg-zinc2 text-zinc4'"
+				v-for="(item, index) in orderTypes" :key="index">
+				{{ item.title }}
+			</div>
+		</div>
 		<view class="content flex justify-start items-center" v-if="ordersLis.length !== 0">
-			<view class="content-item" v-for="(item, index) in ordersLis" :key="index">
+			<view class="content-item" v-for="(item, index) in ordersLis" :key="index" @click="toDetails(item)">
 				<order-card :orderNumber="item.order_sn" :price="item.amount * 0.01"
-					:product_picture="item.items[0].product_picture" :status="item.status"
-					:count="item.items[0].quantity"></order-card>
+					:product_picture="item.items[0].product_picture" :status="getOrderStatus(item.status)"
+					:count="item.items[0].quantity" :pickUpMethod="getPickUpMethod(item.dispatch_mode)" />
 			</view>
 		</view>
 		<view class="empty flex justify-center items-center" v-if="ordersLis.length === 0">
@@ -25,32 +31,97 @@
 import OrderCard from '../../components/orderCard.vue'
 import { orders } from '@/service/index'
 let ordersLis = ref([])
-let orderStatus = ref('')
+let selectType = ref('')
 // 0=待付款
 // 1=已付款待发货
 // 2=已发货带收货
 // 3=已收货完成订单
 // 4=已取消
-let orderType = ref([
-	{ title: '全部', status: null },
+const toDetails = (item) => {
+	console.log(item, `${item.id}`)
+	uni.navigateTo({
+		url: `/pages/home/product_details?productId=${item.id}`
+	});
+}
+const getPickUpMethod = () => {
+	let description;
+
+	switch (deliveryMethod) {
+		case 0:
+			description = '自提';
+			break;
+		case 1:
+			description = '物流';
+			break;
+		case 2:
+			description = '线下活动';
+			break;
+		default:
+			description = '未知配送方式';
+			break;
+	}
+
+	// 返回配送方式描述
+	return description;
+}
+const getOrderStatus = (orderStatus) => {
+	let description;
+
+	switch (orderStatus) {
+		case 0:
+			description = '待付款';
+			break;
+		case 1:
+			description = '已付款待发货';
+			break;
+		case 2:
+			description = '已发货待收货';
+			break;
+		case 3:
+			description = '已收货完成订单';
+			break;
+		case 4:
+			description = '已取消';
+			break;
+		default:
+			description = '未知状态';
+			break;
+	}
+
+	// 这里可以返回描述，或者根据需求执行其他操作
+	return description;
+}
+let orderTypes = ref([
+	{ title: '全部', status: '' },
 	{ title: '待付款', status: 0 },
 	{ title: '已取消', status: 4 },
 	{ title: '待收货', status: 2 },
 	{ title: '已完成', status: 3 },
 
 ])
-let getOrders = async (orderStatus) => {
-	console.log('查询的订单状态', orderStatus)
-	let res = await orders({ status: orderStatus })
-	console.log('订单列表', res)
-	ordersLis.value = res.data
+const handleType = async (item) => {
+	selectType.value = item.status
+	getOrders(selectType.value)
+}
+let getOrders = async (orderStatus) => { //订单列表
+	try {
+		uni.showLoading()
+		console.log('查询的订单状态', orderStatus)
+		let res = await orders({ status: orderStatus })
+		console.log('订单列表', res)
+		ordersLis.value = res.data
+		uni.hideLoading()
+	} catch (err) {
+		console.log(err)
+		uni.hideLoading()
+	}
 }
 onMounted(() => {
-	getOrders(orderStatus.value)
+	getOrders(selectType.value)
 })
 onLoad((options) => {
 	if (options.status) {
-		orderStatus.value = options.status
+		// orderStatus.value = options.status
 	}
 })
 </script>
@@ -72,7 +143,6 @@ onLoad((options) => {
 		width: 100%;
 		flex-direction: column;
 		overflow-y: scroll;
-		padding-top: 60rpx;
 		padding-bottom: 60rpx;
 
 		.content-item {
