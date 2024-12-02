e9083e8e5ef0a3f227a036f90af0a7a7a7ffdae0
SunburstMeoga <1025113986@qq.com>
2024-12-03 01:10:46 +0800

    feaat

--- src/pages/personal/orders.vue
+++ src/pages/personal/orders.vue
@@ -41,7 +41,7 @@ let selectType = ref('')
 const toDetails = (item) => {
 	console.log(item, `${item.id}`)
 	uni.navigateTo({
-		url: `/pages/home/order_details?productId=${item.id}`
+		url: `/pages/home/order_details?productId=${item.order_sn}`
 	});
 }
 const getPickUpMethod = (deliveryMethod) => {
